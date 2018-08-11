2a55379ee0dfaefa66b74ffd16b32a98ade9771e
Ciaran <ciaran.gruber@my.jcu.edu.au>
2018-08-11 16:08:52 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,25 @@
 """
 Ciaran
 """
+
+MIN_LENGTH = 5
+
+
+def is_min_length(text, min_length):
+    """Check if text is of a minimum length"""
+    return len(text) >= min_length
+
+
+def main():
+    """Check password"""
+    password = ''
+    while not is_min_length(password, MIN_LENGTH):
+        password = input('Please type in your password')
+        if is_min_length(password, MIN_LENGTH):
+            for x in range(len(password)):
+                print('*', end='')
+        else:
+            print('Please type in an appropriate password')
+
+
+main()
